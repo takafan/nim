cd6b4c343517978acc76bd135c2634ef204212d9
takafan <takafan@163.com>
2011-07-28 18:48:58 +0800

    a

--- lib/nim/nim_core.rb
+++ lib/nim/nim_core.rb
@@ -1,55 +1,117 @@
 module Nim
   class Nim
 
-    #游戏规则：分别为3，4，5，6颗的四摊豆子，两个玩家定下先后手，轮流拣，一次可拿任意一摊里的至少一颗，拣掉最后一粒豆子的玩家判负。 
+    #游戏规则：分成几摊的豆子，两个玩家轮流拣，一次可拿任意一摊里的至少一颗，拣掉最后一粒豆子的玩家判负。 
     
-    def initialize(hand = 2)
-      init_mat_and_unsafe_positions
+    def initialize
+      puts %q{
+You and Q take turns nim beans from distinct heaps, 
+On each turn, you must nim at least one bean, and may nim any number of beans provided they all come from the same heap.
+
+Whom nim the last one bean were *lost*
+
+}
+
+      a = [1,3,5,7]
+      b = [3,4,5,6]
+      puts "heap 1 (#{a.inject{|x, sum| x + sum}} beans)"
+      draw0(a)
+      puts "heap 2 (#{b.inject{|x, sum| x + sum}} beans)"
+      draw0(b)
       
-      @unsafe_positions.each{|x| puts x.join ' '}
+      while true
+        print 'choose the heap 1/2: '
+        heap = STDIN.gets.to_i
+        if [1,2].include?(heap)
+          @mat = heap == 1 ? a : b
+          break 
+        end
+        puts %q{    input: 1 or 2
+}
+      end
+      
+      init_unsafe_positions
+      
+      while true
+        print 'nim first or second 1/2: '
+        gets = STDIN.gets
+        if gets.strip == "unsafe"
+          @unsafe_positions.each{|x| puts x.join ' '}
+          draw_mat
+        else
+          hand = gets.to_i
+          break if [1,2].include?(hand)
+          puts %q{    input: 1 or 2
+}
+        end
+      end
+      puts 
       
       ai_pick if hand == 2
+      
       while true
-        print 'You nim:'
-        man_pick STDIN.gets
+      
+        while true
+          print 'you nim: '
+          args = STDIN.gets.split ' '
+          
+          if args.size == 1
+            iarg0 = args[0].to_i
+            
+            if iarg0 > 0 and iarg0 < 10 #4
+              pos = iarg0 - 1
+              if (0...@mat.size).include?(pos) and @mat[pos] > 0
+                pick(pos)
+                break
+              end
+            elsif iarg0 > 10 and iarg0 < 100  #45
+              pos = iarg0 / 10 - 1
+              num = iarg0 % 10
+              if (0...@mat.size).include?(pos) and num > 0 and @mat[pos] >= num
+                pick(pos, num)
+                break
+              end
+            end
+            
+          elsif args.size == 2
+            iarg0 = args[0].to_i
+            iarg1 = args[1].to_i
+            
+            if iarg0 > 0 and iarg0 < 10 and iarg1 > 0 and iarg1 < 10  #4 5
+              pos = iarg0 -1
+              num = iarg1
+              if (1...@mat.size).include?(pos) and @mat[pos] >= num
+                pick(pos, num)
+                break
+              end
+            end
+          end
+          puts %q{    invalid arguments.
+    nim least 1 beans from a line.
+    nim 5 beans from line 4, input: 45 or 4 5
+    nim all beans from line 4, input: 4
+}
+        end
+        
         if @mat == [0,0,0,0]
-          puts 'You LOSE :('
+          puts 'you *LOSE* :('
           break
         end
         
         ai_pick
         if @mat == [0,0,0,0]
-          puts 'You WIN :)'
+          puts 'you *WIN* :)'
           break
         end
       end
       
     end
-   
-   
-    def draw_mat
-      @graph = ''
-      space = ' '
-      dot = '.'
-      
-      @mat.each_with_index do |c, i|
-        @graph << "#{space * 4} #{space * (3 - i)} #{(dot + space) * c} \n"
-      end
-      
-      puts @graph
-    end
-    
-    
-    def man_pick(arg)
-      intarg = arg.to_i
-      @mat[intarg/10 - 1] -= intarg%10
-      draw_mat
-    end
     
     
     def ai_pick
-      coll = Hash.new
-      tmp = []
+      #行动集
+      actions = Hash.new
+
       @unsafe_positions.each do |unsafe|
         dup = unsafe.dup
 
@@ -68,34 +130,74 @@ module Nim
         end
         
         if dup.size == 1 and scoped_value > dup.first
-          coll[scoped_index] = scoped_value - dup.shift
+          actions[scoped_index] = scoped_value - dup.shift
         end
       end
       
       #拣取的下标和数量
       pos = num = 0
       
-      if coll.size > 0
+      if actions.size > 0
         #随机挑一个候选执行
-        keys = coll.keys
+        keys = actions.keys
         pos = keys[Random.new.rand(0...keys.size)]
-        num = coll[pos]
+        num = actions[pos]
       else
-        puts 'r'
-        #随机找下标和差额
-        pos = Random.new.rand(0...@mat.size)
+        #值不为0的下标集
+        pos_ex = []
+        @mat.each_with_index do |x, i|
+          pos_ex << i if x > 0
+        end
+        
+        #从不为0的堆中随机找一堆，随机拣取n颗
+        pos = pos_ex[Random.new.rand(0...pos_ex.size)]
         num = Random.new.rand(1..@mat[pos])
       end
-      
-      @mat[pos] -= num
-      
+
       puts "Q nim: #{(pos + 1) * 10 + num}"
+      pick(pos, num)
+    end
+    
+    
+    def pick(pos, num=@mat[pos])
+      @mat[pos] -= num
+      puts
       draw_mat
     end
     
     
-    def init_mat_and_unsafe_positions
-      @mat = [3, 4, 5, 6]
+    def draw_mat
+      draw(@mat)
+    end
+    
+    
+    def draw(mat)
+      graph = ''
+      space = ' '
+      dot = '.'
+      
+      mat.each_with_index do |c, i|
+        graph << "#{space * 4} #{i + 1} #{space * (3 - i)} #{(dot + space) * c} \n"
+      end
+      
+      puts "#{graph}\n"
+    end
+    
+    
+    def draw0(mat)
+      graph = ''
+      space = ' '
+      dot = '.'
+      
+      mat.each_with_index do |c, i|
+        graph << "#{space * 4} #{space * (3 - i)} #{(dot + space) * c} \n"
+      end
+      
+      puts "#{graph}\n"
+    end
+    
+    
+    def init_unsafe_positions
       @unsafe_positions = []
       #初始化阵集
       arr = [] 
@@ -137,9 +239,9 @@ module Nim
         @unsafe_positions << sample unless is_safe
       end
       
+      puts
       draw_mat
     end
     
-    attr_reader :mat, :graph, :unsafe_positions
   end
 end
