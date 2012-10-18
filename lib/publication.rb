276f32e365e0479210d892c4917c392f3ac0df9d
takafan <takafan@163.com>
2012-10-18 11:48:11 +0800

    a

--- lib/publication.rb
+++ lib/publication.rb
@@ -2,7 +2,7 @@ require 'faye'
 require 'eventmachine'
 
 EM.run do
-  client = Faye::Client.new('http://localhost:3000/faye')
+  client = Faye::Client.new('http://localhost:5959/faye')
 
   client.publish('/foo', 'text' => 'Hello world')
 end
--- lib/subscription.rb
+++ lib/subscription.rb
@@ -2,7 +2,7 @@ require 'faye'
 require 'eventmachine'
 
 EM.run do
-  client = Faye::Client.new('http://localhost:3000/faye')
+  client = Faye::Client.new('http://localhost:5959/faye')
 
   client.subscribe('/foo') do |message|
     puts message.inspect
